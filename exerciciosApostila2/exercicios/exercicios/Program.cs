ce1c9d0709a32b538344238eb1957f5feb178d48
Leandro Higor <leo1309melo@outlook.com>
2021-02-23 00:12:49 -0300

    Impar ou Par

--- exerciciosApostila2/exercicios/exercicios/Program.cs
+++ exerciciosApostila2/exercicios/exercicios/Program.cs
@@ -6,7 +6,24 @@ namespace exercicios
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Exercício 1
+            // Elabore um programa em console, c#, e requisite ao operador um valor numérico. faça uma rotina para verificar se o número é par ou ímpar, imprima o resultado no console.
+
+            int getValue = 0;
+            int valueAux = 0;
+
+            Console.Write("Digite um valor inteiro para saber se o número é impar ou par: ");
+            getValue = int.Parse(Console.ReadLine());
+
+            while (getValue < 0) {
+                Console.Write("Por favor, insira um número maior que 0: ");
+                getValue = int.Parse(Console.ReadLine());
+            }
+
+            valueAux = getValue % 2;
+            Console.Write( (valueAux == 1) ? "O número " + getValue + " é impar" : "O número " + getValue + " é par" );
+            Console.ReadKey();
+            
         }
     }
 }
