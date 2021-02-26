3ec8765d75032947cc0b9f99e147890ee4fec21f
Leandro Higor <leo1309melo@outlook.com>
2021-02-25 23:54:25 -0300

    exercicio 2 apostila 2

--- exerciciosApostila2/exercicios/exercicios/Program.cs
+++ exerciciosApostila2/exercicios/exercicios/Program.cs
@@ -63,14 +63,49 @@ namespace exercicios
                     }
 
                     valueAux = getValue % 2;
-                    Console.Write((valueAux == 1) ? "O número " + getValue + " é impar" : "O número " + getValue + " é par");
-                    Console.ReadKey();
+                    Console.Write((valueAux == 1) ? "O número " + getValue + " é impar" : "O número " + getValue + " é par")
 
                     Console.WriteLine("================================================");
-                break;
+                    break;
+
+                case 2: 
+                    Console.WriteLine("================================================");
+                    Console.WriteLine("Exercício 2");
+                    Console.WriteLine("Elabore um programa que requisite ao operador 3 números. Desenvolva uma rotina que encontre o menor número e imprima-o no console." +
+                        " Valide para que nenhum número seja igual ao outro.");
+
+                    int valueAux = 0;
+                    int value1   = 0;
+                    int value2   = 0;
+                    int value3   = 0;
+
+                    Console.Write("Digite o primeiro valor: ");
+                    value1 = int.Parse(Console.ReadLine());
+
+                    valueAux = (value1 < valueAux) ? value1 : valueAux;
+
+                    Console.Write("Digite o segundo valor: ");
+                    value2 = int.Parse(Console.ReadLine());
+                    while (value2 == value1) {
+                        Console.Write("Digite o segundo valor que seja diferente dos outros: ");
+                        value2 = int.Parse(Console.ReadLine());
+                    }
+
+                    valueAux = (value2 < valueAux) ? value2 : valueAux;
+
+                    Console.Write("Digite o terceiro valor: ");
+                    value3 = int.Parse(Console.ReadLine());
+                    while (value3 == value1 || value3 == value2) {
+                        Console.Write("Digite o terceiro valor que seja diferente dos outros: ");
+                        value3 = int.Parse(Console.ReadLine());
+                    }
+
+                    valueAux = (value3 < valueAux) ? value3 : valueAux;
+                    Console.WriteLine("O menor valor digitado fo: " + valueAux);
+                    break;
             }
 
-
+            Console.ReadKey();
         }
     }
 }
